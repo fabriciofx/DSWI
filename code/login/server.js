ad8effdc4a547ce2156b75e26336b06384c9d3a7
Fabrício Barros Cabral <fabriciofx@gmail.com>
2023-04-21 04:39:56 -0300

    Formata código

--- code/login/server.js
+++ code/login/server.js
@@ -8,28 +8,37 @@ app.use(body_parser.urlencoded({extended: false }));
 
 // Ao requisitar com o verbo GET com path '/filename', envie a página
 // 'filename'.
-app.get('/:filename', function(req, res) {
-    const filename = req.params.filename;
-    res.sendFile(filename, { root: __dirname });
-});
+app.get(
+    '/:filename',
+    function(req, res) {
+        const filename = req.params.filename;
+        res.sendFile(filename, { root: __dirname });
+    }
+);
 
 // Ao requisitar com o verbo GET com path '/login', mostre o conteúdo dos
 // parâmetros 'usuario' e 'senha'
-app.get('/login', function(req, res) {
-    const usuario = req.query.usuario;
-    const senha = req.query.senha;
-    res.send('GET: usuario: ' + usuario + ' senha: ' + senha);
-});
+app.get(
+    '/login',
+    function(req, res) {
+        const usuario = req.query.usuario;
+        const senha = req.query.senha;
+        res.send('GET: usuario: ' + usuario + ' senha: ' + senha);
+    }
+);
 
 // Ao requisitar com o verbo POST com path '/login', mostre o conteúdo dos
 // parâmetros 'usuario' e 'senha'
-app.post('/login', function(req, res) {
-    const usuario = req.body.usuario;
-    const senha = req.body.senha;
-    res.send('POST: usuario: ' + usuario + ' senha: ' + senha);
-});
+app.post(
+    '/login',
+    function(req, res) {
+        const usuario = req.body.usuario;
+        const senha = req.body.senha;
+        res.send('POST: usuario: ' + usuario + ' senha: ' + senha);
+    }
+);
 
-// Executar o serviço na porta 8080
+// Executa o serviço na porta 8080
 app.listen(port);
 
 // Logging
